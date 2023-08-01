5f14a465409617da3a37bf23f4bdcfd733cb7eb2
Elalitareq <elalitareq@gmail.com>
2023-08-01 13:09:42 +0300

    tested

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState, useRef } from "react";
 import { motion } from "framer-motion";
 import CustomBtn from "./custom_btn";
 import Image from "next/image";
-import rocket from "../app/assets/vector/rocket.svg";
+import rocket from "../app/assets/vector/rocket2.svg";
+import landing from "../app/assets/vector/landing.svg";
+import stats from "../app/assets/vector/stats.svg";
 
 const Hero = () => {
   const containerRef = useRef(null);
@@ -28,20 +30,19 @@ const Hero = () => {
 
   const rocketAnimation = {
     initial: {
-      x: 200,
+      x: 180,
       y: 0,
       opacity: 0,
       rotate: 0,
       scale: 0.6,
     },
     animate: {
-      y: [0, -150, -300, 100], // Adjusted the final y position to land at the bottom-center
-      opacity: [0, 1, 1, 1],
-      scale: [0.6, 1, 0.6, 1],
+      y: [-150, -300, -150], // Adjusted the final y position to land at the bottom-center
+      opacity: [1, 1, 1],
       transition: {
         duration: 8,
         ease: "easeInOut",
-        times: [0, 0.3, 0.6, 1],
+        times: [0, 0.2, 1],
       },
     },
     exit: {
@@ -56,7 +57,7 @@ const Hero = () => {
   return (
     <section
       ref={containerRef}
-      className="relative min-h-[calc(100vh-80px)] flex flex-col  justify-between overflow-hidden"
+      className="relative min-h-[calc(100vh-60px)] flex flex-col  justify-between overflow-hidden"
     >
       <motion.main
         animate={{ opacity: 1, y: 50 }}
@@ -64,7 +65,7 @@ const Hero = () => {
         initial={{ opacity: 0 }}
         className="w-2/3 md:w-3/4 px-3"
       >
-        <h1 className="text-center max-sm:text-start max-sm:text-5 text-4xl uppercase font-extrabold">
+        <h1 className="text-center max-sm:text-start max-sm:text-5 text-3xl uppercase font-extrabold">
           this is a digital marketing app
         </h1>
         <p className="text-center max-sm:text-start mx-auto w-2/3 sm:w-full lg:w-3/5 max-sm:w-full mt-2">
@@ -73,7 +74,7 @@ const Hero = () => {
         </p>
         <CustomBtn name="test" route="#" />
       </motion.main>
-      <div className="">
+      <div className="relative">
         <motion.div
           initial="initial"
           animate="animate"
@@ -93,6 +94,20 @@ const Hero = () => {
             <Image src={rocket} width="200" height="220" alt="rocket" />
           </motion.div>
         </motion.div>
+        <Image
+          src={landing}
+          width={"100%"}
+          className="absolute bottom-0 w-full h-auto"
+          height="auto"
+          alt="landing"
+        />
+        <Image
+          src={stats}
+          width={"30%"}
+          className="absolute bottom-0 w-20 left-1/3 h-auto"
+          height="auto"
+          alt="landing"
+        />
       </div>
     </section>
   );

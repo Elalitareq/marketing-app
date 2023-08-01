1942a02acfb6499b246997ca132b2da4492d89eb
Elalitareq <elalitareq@gmail.com>
2023-08-01 12:49:44 +0300

    tested

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -28,22 +28,20 @@ const Hero = () => {
 
   const rocketAnimation = {
     initial: {
-      x: -100,
+      x: 200,
       y: 0,
       opacity: 0,
       rotate: 0,
       scale: 0.6,
     },
     animate: {
-      x: [50, 250, 50, -150, 50, 150],
-      y: [0, -150, -300, -150, 200, 90], // Adjusted the final y position to land at the bottom-center
-      opacity: [0, 1, 1, 1, 0, 1],
-      rotate: [0, 0, 360, 360],
-      scale: [0.6, 1, 0.6, 0.6, 0.6, 0.6],
+      y: [0, -150, -300, 100], // Adjusted the final y position to land at the bottom-center
+      opacity: [0, 1, 1, 1],
+      scale: [0.6, 1, 0.6, 1],
       transition: {
         duration: 8,
         ease: "easeInOut",
-        times: [0, 0.2, 0.5, 0.8, 0.9, 1],
+        times: [0, 0.3, 0.6, 1],
       },
     },
     exit: {
@@ -58,36 +56,33 @@ const Hero = () => {
   return (
     <section
       ref={containerRef}
-      className="relative min-h-[calc(100vh-80px)] flex flex-col items-center justify-between overflow-hidden"
+      className="relative min-h-[calc(100vh-80px)] flex flex-col  justify-between overflow-hidden"
     >
       <motion.main
         animate={{ opacity: 1, y: 50 }}
         transition={{ ease: "easeOut", duration: 0.56 }}
         initial={{ opacity: 0 }}
-        className="w-2/3 md:w-3/4 max-sm:w-full px-3"
+        className="w-2/3 md:w-3/4 px-3"
       >
         <h1 className="text-center max-sm:text-start max-sm:text-5 text-4xl uppercase font-extrabold">
           this is a digital marketing app
         </h1>
         <p className="text-center max-sm:text-start mx-auto w-2/3 sm:w-full lg:w-3/5 max-sm:w-full mt-2">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis,
-          at. Tempore doloremque beatae quia architecto porro consequuntur
-          neque, similique enim autem temporibus,
+          at.
         </p>
         <CustomBtn name="test" route="#" />
       </motion.main>
-      <div className="absolute bottom-0 left-0 right-0 -z-10 ">
+      <div className="">
         <motion.div
           initial="initial"
           animate="animate"
           exit="exit"
           variants={rocketAnimation}
-          whileHover={{
-            scale: 1.2,
-            rotate: 360,
-            transition: { duration: 0.5 },
+          whileTap={{
+            translateY: -80,
+            transition: { duration: 0.8 },
           }}
-          whileTap={{ scale: 0.8 }}
           style={{ originX: 0.5, originY: 0.8 }} // To rotate from the bottom-center
         >
           <motion.div
--- src/components/storyCard.jsx
+++ src/components/storyCard.jsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 
 const StoryCard = () => {
 	return (
-		<article className="w-[300px] h-[360px]  rounded-xl shadow-lg hover:shadow-xl  transition duration-300 cursor-pointer overflow-hidden hover:scale-105 ">
-			<div className="h-1/2 w-full ">
+		<article className="w-[150px] rounded-xl shadow-lg hover:shadow-xl  transition duration-300 cursor-pointer overflow-hidden hover:scale-105 ">
+			<div className=" w-full ">
 				<Image
 					className="object-cover"
 					src={'https://i.ibb.co/pJv84y2/tiktok.webp'}
@@ -13,10 +13,9 @@ const StoryCard = () => {
 					width={1000}
 				/>
 			</div>
-			<div className="text-gray-900 font-medium tracking-widest  p-4 leading-5 h-1/2 text-sm">
+			<div className="text-gray-900 font-medium leading-4 p-2   text-xs overflow-hidden tracking-tight">
 				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-				vel labore nulla, ut maiores, cumque minima rem quis alias nobis
-				quas
+				vel labore nulla, ut maiores.
 			</div>
 		</article>
 	);
